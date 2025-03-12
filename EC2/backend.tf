2dc395444ce9b8fbbe9f97c342a0b09bd61b0c91
Sudharshan12313 <167956857+Sudharshan12313@users.noreply.github.com>
2025-03-12 09:09:06 +0530

    Update backend.tf

--- EC2/backend.tf
+++ EC2/backend.tf
@@ -1,8 +1,8 @@
-terraform{
+/*terraform{
   backend "s3" {
     bucket         = "nsh-state-new"
     key            = "dev/EC2/terraform.tfstate"
     region         = "us-west-2"
     encrypt        = true
   }
-}
+}*/
