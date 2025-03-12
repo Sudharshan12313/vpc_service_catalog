66b191718580f748806cb1e6c86f2885ca579b57
Sudharshan12313 <167956857+Sudharshan12313@users.noreply.github.com>
2025-03-12 09:09:31 +0530

    Update backend.tf

--- VPC/backend.tf
+++ VPC/backend.tf
@@ -1,8 +1,8 @@
-terraform{
+/*terraform{
   backend "s3" {
     bucket         = "nsh-state-new"
     key            = "dev/VPC/terraform.tfstate"
     region         = "us-west-2"
     encrypt        = true
   }
-}
+}*/
